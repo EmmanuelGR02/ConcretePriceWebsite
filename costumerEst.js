2fdae1654bb9d7850e2ade8101695737dde32ab3
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-07-22 15:22:54 -0400

    costumer page 70% done

--- costumerEst.js
+++ costumerEst.js
@@ -1,13 +1,17 @@
-function yardsMain() {
+function estMain() {
     // declare the measurements to variables
     var length = document.getElementById("yardLength").value;
     var width = document.getElementById("yardWidth").value;
     var depth = document.getElementById("yardDepth").value;
+    var jobType = document.getElementById("estType").value;
+    var yesNo = document.getElementById("estRemoval").value;
+    var price;
     var totalYards;
 
 
     checkInvalidInput();
     calculateYards();
+    calculatePrice();
 
     // if totalYards is not a number, displays error message
     if (totalYards >= 0) {
@@ -48,5 +52,45 @@ function yardsMain() {
         } else if (depth == "") {
             document.getElementById("invalidDepth").style.display="block"; 
         }
+    }
+
+    function calculatePrice(){
+        let tempPrice;
+        let estPrice;
+
+        if (totalYards < 5) {
+            tempPrice= totalYards * 300;
+        } else{ 
+            tempPrice = totalYards * 170;
+        }
+
+        if (totalYards > 0.5 && totalYards < 1) {
+            if (yesNo == "YES") {
+                price = tempPrice + 1500;
+                estPrice = price + 1200;
+                document.getElementById("estimate").innerHTML = "Estimated Price for " + " " +length + "ft. x " + width + "ft. x " + depth + "in. " + jobType + ": $" + price + " - " + estPrice;
+            } else {
+                price = tempPrice + 1000;
+                estPrice = price + 900;
+                document.getElementById("estimate").innerHTML = "Estimated Price for " + " " +length + "ft. x " + width + "ft. x " + depth + "in. " + jobType + ": $" + price + " - " + estPrice;
+            }
+        } else if (totalYards > 1 && totalYards < 3) {
+            if (yesNo == "YES") {
+                price = tempPrice + 1700;
+                estPrice = price + 1500;
+                document.getElementById("estimate").innerHTML = "Estimated Price for " + " " +length + "ft. x " + width + "ft. x " + depth + "in. " + jobType + ": $" + price + " - " + estPrice;
+            } else {
+                price = tempPrice + 1500;
+                estPrice = price + 1400;
+                document.getElementById("estimate").innerHTML = "Estimated Price for " + " " +length + "ft. x " + width + "ft. x " + depth + "in. " + jobType + ": $" + price + " - " + estPrice;
+            }
+        } else if (totalYards > 3 && totalYards < 6) {
+            if (yesNo == "YES") {
+                price = tempPrice + 2200;
+                estPrice = price + 2000;
+                document.getElementById("estimate").innerHTML = "Estimated Price for " + " " +length + "ft. x " + width + "ft. x " + depth + "in. " + jobType + ": $" + price + " - " + estPrice;
+            }
+        }
+
     }
 }
